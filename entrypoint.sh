9eeda757c008b80d98b15132a6e8d63fae401390
Eric Platon <zaraki@gmx.com>
2018-09-26 17:58:33 +0900

    Refactoring
    * Commands layout for readability.
    * Turn comments into echo.

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,25 +25,39 @@ CERTPATH=/etc/letsencrypt/live/$(echo $DOMAINS | cut -f1 -d',')
 
 ls $CERTPATH || exit 1
 
+
+echo "Update secret..."
 cat /secret-patch-template.json | \
 	sed "s/NAMESPACE/${NAMESPACE}/" | \
 	sed "s/NAME/${SECRET}/" | \
 	sed "s/TLSCERT/$(cat ${CERTPATH}/fullchain.pem | base64 | tr -d '\n')/" | \
 	sed "s/TLSKEY/$(cat ${CERTPATH}/privkey.pem |  base64 | tr -d '\n')/" \
 	> /secret-patch.json
-
 ls /secret-patch.json || exit 1
 
-# update secret
-curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
+curl -v --fail \
+  -k --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+  -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+  -H "Accept: application/json, */*" \
+  -H "Content-Type: application/strategic-merge-patch+json" \
+  -X PATCH \
+  -d @/secret-patch.json \
+  https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
 
+
+echo "Update the pod spec on ingress deployment to trigger redeploy..."
 cat /deployment-patch-template.json | \
 	sed "s/TLSUPDATED/$(date)/" | \
 	sed "s/NAMESPACE/${NAMESPACE}/" | \
 	sed "s/NAME/${DEPLOYMENT}/" \
 	> /deployment-patch.json
-
 ls /deployment-patch.json || exit 1
 
-# update pod spec on ingress deployment to trigger redeploy
-curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/deployment-patch.json https://kubernetes/apis/extensions/v1beta1/namespaces/${NAMESPACE}/deployments/${DEPLOYMENT}
+curl --fail \
+  -k --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+  -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+  -H "Accept: application/json, */*" \
+  -H "Content-Type: application/strategic-merge-patch+json" \
+  -X PATCH  \
+  -d @/deployment-patch.json \
+  https://kubernetes/apis/extensions/v1beta1/namespaces/${NAMESPACE}/deployments/${DEPLOYMENT}
